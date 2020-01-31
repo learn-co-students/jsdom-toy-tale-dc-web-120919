d6462f51c1ea84a4d70c74583fe458b459f0d9e1
John Louis Rumingan <jmrumingan@gmail.com>
2020-01-31 15:27:28 -0500

    Added like functions

--- src/index.js
+++ src/index.js
@@ -24,12 +24,98 @@ function getToyForm(){
   return document.querySelector('.add-toy-form')
 }
 
+
+function getToyCollection(){
+  return document.getElementById('toy-collection');
+}
+
+function getToys(){
+  
+  fetch('http://localhost:3000/toys')
+  .then(response => response.json())
+  .then(toys => {
+    console.log(toys);
+    toys.forEach(toy => buildToyCard(toy));
+  })
+}
+
+function buildToyCard(toy){
+  
+  let toyGallery = getToyCollection();
+  let toyCard = document.createElement('div');
+  toyCard.classList += "card";
+  toyCard.dataset.id = toy.id;
+  let newName = document.createElement('h2');
+  newName.innerHTML = toy.name;
+  let newImage = document.createElement('img');
+  newImage.className = "toy-avatar";
+  newImage.src = toy.image;
+  let newLikes = document.createElement('p');
+  newLikes.innerText = `${toy.likes} Likes`;
+  newLikes.dataset.id = `like-${toy.id}`;
+  newLikes.id = `like-${toy.id}`;
+  let newButton = document.createElement('button');
+  newButton.className = 'like-btn';
+  newButton.innerText = 'Like';
+  newButton.dataset.id = toy.id;
+  newButton.id = `like-button-${toy.id}`;
+  
+  toyGallery.appendChild(toyCard);
+  toyCard.appendChild(newName);
+  toyCard.appendChild(newImage);
+  toyCard.appendChild(newLikes);
+  toyCard.appendChild(newButton);
+  
+  newButton.addEventListener('click', currentLikes);
+}
+
+function currentLikes(event){
+  console.log("I'm clicked!");
+  let currentLikes = event.target.dataset.id;
+  let toyID = parseInt(currentLikes);
+
+  //fetches the current likes 
+  let likeCounter = "hi";
+  
+  fetch(`http://localhost:3000/toys/${toyID}`)
+  .then(response => response.json())
+  .then(toy => {
+    console.log(toy);
+    // debugger;
+    likeCounter = toy.likes;
+    let toyID = toy.id;
+    likeCounter++;
+    patchLikes(likeCounter, toyID);
+
+  });
+
+  // let stringLikes = String(likeCounter);
+}
+
+function patchLikes(likeCounter, toyID){
+  
+  fetch(`http://localhost:3000/toys/${toyID}`, {
+    method: 'PATCH',
+    body: JSON.stringify({
+      likes: likeCounter
+       }), //variable likes here
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+}
+ 
+
+
 function newToyHandler(event){
   event.preventDefault();
   console.log(event);
   let toyName = event.target.name.value;
   let toyImage = event.target.image.value;
-  let newToy = {name: toyName, image: toyImage, likes: 0}
+  let newToy = {likes: 0, name: toyName, image: toyImage}
   //post this info to the JSON server 
   let newPost = fetch('http://localhost:3000/toys', {
     method: "POST",
@@ -43,40 +129,3 @@ function newToyHandler(event){
   
 }
 
-function getToyCollection(){
-  return document.getElementById('toy-collection');
-}
-
-function getToys(){
-
-  fetch('http://localhost:3000/toys')
-    .then(response => response.json())
-    .then(toys => {
-      console.log(toys);
-      toys.forEach(toy => buildToyCard(toy));
-    })
-}
-
-function buildToyCard(toy){
-
-  let toyGallery = getToyCollection();
-  let toyCard = document.createElement('div');
-  toyCard.classList += "card";
-  toyCard.dataset.id = toy.id;
-  let newName = document.createElement('h2');
-  newName.innerHTML = toy.name;
-  let newImage = document.createElement('img');
-  newImage.className = "toy-avatar";
-  newImage.src = toy.image;
-  let newLikes = document.createElement('p');
-  newLikes.innerText = `${toy.likes} Likes`;
-  let newButton = document.createElement('button');
-  newButton.className = 'like-btn';
-  newButton.innerText = 'Like';
-
-  toyGallery.appendChild(toyCard);
-  toyCard.appendChild(newName);
-  toyCard.appendChild(newImage);
-  toyCard.appendChild(newLikes);
-  toyCard.appendChild(newButton);
-}
